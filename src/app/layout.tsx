6a08b49d96e08556a4040a27d2eb2396204096d3
Mesoon <masonlshields@gmail.com>
2025-05-31 18:50:28 -0400

    WIDE RANGING style changes. What fun!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Source_Code_Pro } from "next/font/google";
 import TheMatrix from "./matrixwrapper";
 import "./css/globals.css";
+import Image from "next/image";
+import wordart from '../../public/wordart.png';
 
 const sourceCodePro = Source_Code_Pro({
   subsets: ["latin"],
@@ -23,7 +25,29 @@ export default function RootLayout({
         className={`${sourceCodePro.className} antialiased`}
       >
         <TheMatrix>
-          {children}
+          <div className="z-10 w-[100%]">
+            <div className="absolute min-h-screen h-auto bg-black absolute text-teal-200 p-5 border-x-2 border-violet-600 w-[75%] md:w-[50%] mx-[12.5%] md:mx-[25%] content-container">
+              <div className="flex justify-center items-center">
+                  <Image
+                    src={wordart}
+                    alt="wordart"
+                    style={{width: "50%", height: "auto"}}
+                    className="object-contain"
+                  />
+              </div>
+              <div className="text-xl text-pink-400 mb-10 underline underline-offset-4 flex items-center justify-around w-[100%]">
+                <a href="/portfolio_v2/">Home</a>
+                <a className="cursor-default">Resume & Portfolio</a>
+                <a className="cursor-default">Blog</a>
+                <a className="cursor-default">Web Thingies</a>
+              </div>
+              {children}
+              <div className="mt-20 text-violet-600 text-center text-xs">
+                <p className="mb-1">Copyright &copy; 2025 Mason Shields</p>
+                <p>You are free to use any code on this website, but ask if you want to use content!</p>
+              </div>
+            </div>
+          </div>
         </TheMatrix>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
-import Image from "next/image";
-import wordart from '../../public/wordart.png';
+import Image from "next/image"
+import faceimage from "../../public/masonimage.jpg"
 
-export default function Home() {
-  return (
-    <div className="z-10 absolute w-[100%] h-[100%]">
-      <div className="bg-black absolute text-teal-200 p-5 border-x-2 border-violet-600 w-[75%] md:w-[50%] h-[100%] mx-[12.5%] md:mx-[25%] content-container">
-        <div className="relative w-[100%] h-[25%]">
-            <Image
-              src={wordart}
-              alt="wordart"
-              fill
-              className="object-contain"
-            />
+export default function Home(){
+    return(
+        <div className="text-center m-5">
+            <p className="mb-5">Hello there! I'm Mason Shields, welcome to my <span className="underline underline-offset-3">(under construction)</span> portfolio website!</p>
+            <p className="mb-5">I want to use this place as somewhere where I can document my CS career, ramble on about my code among other things, keep people posted on what I'm up to, and maybe make someone's day a little better!</p>
+            <div className="flex justify-center">
+                <Image 
+                        src={faceimage}
+                        alt="face image"
+                        width={300}
+                        height={300}
+                        className="static mb-2 border-20 border-violet-600 object-contain"
+                        style={{borderStyle: "ridge"}}
+                />
+            </div>
+            <p className="text-emerald-500 mb-10">Caption: Me, for reference</p>
+            <p className="mb-5">As a developer, I primarily work with full stack web development with a specialization in backend. I use React, Next.js, and Tailwind for a lot of my work, and am most familiar with Python and C++ for heavy duty DSA things.</p>
+            <p className="mb-5">If you want a longer "brag sheet" than that, read my resume! I like to think I'm a humble guy, and I'd rather have my website showcase the things I build than list the lines on my resume.</p>
+            <p className="text-emerald-500 mb-2">
+                Where to Find Me:
+            </p>
+            <div className="flex w-[100%] justify-center">
+                <div className="flex justify-around w-[50%] underline underline-offset-3 text-pink-400">
+                    <a href="https://github.com/Mesoon5642" target="_blank">Github</a>
+                    <a href="http://www.linkedin.com/in/mason-shields-713613289" target="_blank">LinkedIn</a>
+                    <a href="mailto:masonlshields@gmail.com" target="_blank">Email</a>
+                </div>
+            </div>
         </div>
-        <p>Hello World!</p>
-      </div>
-    </div>
-
-  );
+    )
 }
